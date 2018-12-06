5b9a4d39257a7df77db0fc8fafd6b95a0fd847a9
Andrei Onoie <>
2018-12-06 14:40:56 +0000

    completed L8

--- L8_DisjointSets_Kruskal/lab8-disjoint.cpp
+++ L8_DisjointSets_Kruskal/lab8-disjoint.cpp
@@ -8,6 +8,13 @@
 
 #define DEMO_SIZE 10
 
+#define AVG_CASE_TRIALS 5
+#define DIM_MIN 100
+#define DIM_MAX 10000
+#define STEP_SIZE 100
+#define RANGE_MIN 1
+#define RANGE_MAX 100000
+
 Profiler profiler("Disjoint_Sets_Kruskal");
 
 typedef struct {
@@ -23,8 +30,9 @@ typedef struct TreeNode {
 } TreeNode;
 
 
-void		demo();
+void demo();
 void testKruskal();
+void evaluateEffort();
 
 unsigned int getRandom(int max);
 
@@ -43,10 +51,14 @@ void		printSet(TreeNode* x);
 Edge* MST_Kruskal(Edge** edges, int E, int V);
 Edge** buildRandomEdges(int V);
 
+
+unsigned int countOperations = 0;
+
 int main() {
 	srand48(time(NULL));
-	//demo();
+	demo();
 	testKruskal();
+	//evaluateEffort();
 	return 0;
 }
 
@@ -106,6 +118,49 @@ void demo() {
 	}
 }
 
+void testKruskal() {
+	int V = 9, E = 16; // from 0 to 8
+	Edge **edges = (Edge**) calloc(E, sizeof(Edge*));
+	
+	edges[0] = newEdge(0, 1, 8);
+	edges[1] = newEdge(0, 2, 12);
+	edges[2] = newEdge(1, 2, 13);
+	edges[3] = newEdge(1, 3, 25);
+	edges[4] = newEdge(1, 4, 9);
+	edges[5] = newEdge(2, 3, 14);
+	edges[6] = newEdge(2, 6, 21);
+	edges[7] = newEdge(3, 4, 20);
+	edges[8] = newEdge(3, 5, 8);
+	edges[9] = newEdge(3, 6, 12);
+	edges[10] = newEdge(3, 7, 12);
+	edges[11] = newEdge(3, 8, 16);
+	edges[12] = newEdge(4, 5, 19);
+	edges[13] = newEdge(5, 7, 11);
+	edges[14] = newEdge(6, 8, 11);
+	edges[15] = newEdge(7, 8, 9);
+	
+	printf("\n\nMST edges are: ");
+	MST_Kruskal(edges, E, V);
+}
+
+void evaluateEffort() {
+	Edge** edges;
+	
+	for (int n = DIM_MIN; n <= DIM_MAX; n += STEP_SIZE) {
+		edges = buildRandomEdges(n);
+		countOperations = 0;
+		MST_Kruskal(edges, n*4, n);
+		profiler.countOperation("Kruskal", n, countOperations);
+		
+		for (int i=0; i < n; i++)
+			free(edges[i]);
+			
+		free(edges);
+	}
+	
+	profiler.showReport();
+}
+
 unsigned int getRandom(int max) {
 	double d = drand48();  // gives you a double from 0 to 1
 	double val = d * max;	  // gives you a double from 0 to max
@@ -116,7 +171,7 @@ unsigned int getRandom(int max) {
 int cmpEdge(const void* a, const void* b) {
 	Edge *e1 = *(Edge**) a;
 	Edge *e2 = *(Edge**) b;
-	
+	countOperations++;
 	return (e1->w - e2->w);	
 }
 
@@ -145,12 +200,17 @@ TreeNode* MAKE_SET(int key) {
 	x->parent = x;
 	x->next = x;	// for getting set elements
 	x->rank = 0;
+	countOperations += 5;
 	return x;
 }
 
 TreeNode* FIND_SET(TreeNode* x) {
-	if (x != x->parent)
+	countOperations++;
+	
+	if (x != x->parent) {
 		x->parent = FIND_SET(x->parent);
+		countOperations += 2;
+	}
 		
 	return x->parent;
 }
@@ -158,10 +218,14 @@ TreeNode* FIND_SET(TreeNode* x) {
 void LINK(TreeNode* x, TreeNode* y) {
 	if (x->rank > y->rank) {
 		y->parent = x;
+		countOperations += 2;
 	} else {
 		x->parent = y;
-		if (x->rank == y->rank)
+		if (x->rank == y->rank) {
 			y->rank++;
+			countOperations++;
+		}
+		countOperations += 3;
 	}
 	
 	swapNext(x, y);	// for getting set elements
@@ -192,15 +256,17 @@ Edge* MST_Kruskal(Edge** edges, int E, int V) {
 		tree[i] = MAKE_SET(i);	
 	
 	qsort(edges, E, sizeof(Edge*), cmpEdge);
-	for (int i=0; i < E; i++) {
-		printf("%d<->%d[%d]\n", edges[i]->u, edges[i]->v, edges[i]->w);
-	}
-	printf("\n\n");
+	// for (int i=0; i < E; i++) {
+	// 	printf("%d<->%d[%d]\n", edges[i]->u, edges[i]->v, edges[i]->w);
+	// }
+	// printf("\n\n");
 	for (int i=0; i < E; i++) {
 		if (FIND_SET(tree[edges[i]->u]) != FIND_SET(tree[edges[i]->v])) {
 			A[k++] = *edges[i];
+			countOperations++;
 			UNION(tree[edges[i]->u], tree[edges[i]->v]);
 		}
+		countOperations++;
 	}
 	
 	for (int i=0; i < k; i++) {
@@ -210,37 +276,30 @@ Edge* MST_Kruskal(Edge** edges, int E, int V) {
 	return A;
 }
 
-void testKruskal() {
-	int V = 20, E = 16; // from 0 to 8
-	// Edge **edges = (Edge**) calloc(E, sizeof(Edge*));
-	
-	// edges[0] = newEdge(0, 1, 8);
-	// edges[1] = newEdge(0, 2, 12);
-	// edges[2] = newEdge(1, 2, 13);
-	// edges[3] = newEdge(1, 3, 25);
-	// edges[4] = newEdge(1, 4, 9);
-	// edges[5] = newEdge(2, 3, 14);
-	// edges[6] = newEdge(2, 6, 21);
-	// edges[7] = newEdge(3, 4, 20);
-	// edges[8] = newEdge(3, 5, 8);
-	// edges[9] = newEdge(3, 6, 12);
-	// edges[10] = newEdge(3, 7, 12);
-	// edges[11] = newEdge(3, 8, 16);
-	// edges[12] = newEdge(4, 5, 19);
-	// edges[13] = newEdge(5, 7, 11);
-	// edges[14] = newEdge(6, 8, 11);
-	// edges[15] = newEdge(7, 8, 9);
-	Edge **edges = buildRandomEdges(V);
-	MST_Kruskal(edges, V*4, V);
-}
-
 Edge** buildRandomEdges(int V) {
 	Edge **edges = (Edge**) calloc(V*4, sizeof(Edge*));
+	bool foundDuplicate;
 	
 	for (int i=0; i < V*4; i++) {
-		int u = getRandom(V-2);
-		edges[i] = newEdge(u, u + 1 + getRandom(V - u - 2), 1 + getRandom(50));
+		do {
+			int u = getRandom(V-2);
+			edges[i] = newEdge(u, u + 1 + getRandom(V - u - 2), RANGE_MIN + getRandom(RANGE_MAX));
+			
+			foundDuplicate = false;
+			for (int j=0; j < i; j++) {
+				if (edges[i]->u == edges[j]->u && edges[i]->v == edges[j]->v) {
+					foundDuplicate = true;
+					break;
+				}
+				
+				if (edges[i]->u == edges[j]->v && edges[i]->v == edges[j]->u) {
+					foundDuplicate = true;
+					break;
+				}
+			}
+		} while (foundDuplicate);
 	}
 	
 	return edges;
 }
+
